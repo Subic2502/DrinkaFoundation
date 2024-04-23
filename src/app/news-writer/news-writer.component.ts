e0bde3bebb54b56e0490994c8b96f8a74446528f
Subic2502 <uros.subic01@gmail.com>
2024-04-23 21:04:02 +0200

    small push on news-writer component

--- src/app/news-writer/news-writer.component.ts
+++ src/app/news-writer/news-writer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-news-writer',
   templateUrl: './news-writer.component.html',
   styleUrl: './news-writer.component.css'
 })
-export class NewsWriterComponent {
+export class NewsWriterComponent implements OnInit{
+  selectedFiles: File[] = [];
+
+  ngOnInit(): void {
+    
+  }
+
+  onFilesSelected(event: any): void {
+    const files: FileList = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      this.selectedFiles.push(files[i]);
+    }
+  }
+
+  resetFiles(): void {
+    this.selectedFiles = [];
+  }
 
 }

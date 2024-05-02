70e8b8b32dbb0a76f67182de9c974c0ffc0ea84c
Subic2502 <uros.subic01@gmail.com>
2024-05-02 18:45:17 +0200

    news page viewer properly fetching news

--- angular/src/app/news-viewer-page/news-viewer-page.component.ts
+++ angular/src/app/news-viewer-page/news-viewer-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from '../Data/News';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-news-viewer-page',
   templateUrl: './news-viewer-page.component.html',
   styleUrl: './news-viewer-page.component.css'
 })
-export class NewsViewerPageComponent {
+export class NewsViewerPageComponent implements OnInit {
+
+  news: News | undefined;
+
+  constructor(private http: HttpClient,private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    console.log("uslo u oninit");
+    const id = this.route.snapshot.queryParams['id'];
+    if (id) {
+      console.log("ID:"+id);
+      this.http.get<News>(`http://localhost:5024/api/News/${id}`)
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching news', error);
+          return throwError(() => new Error('Error fetching news'));
+        })
+      )
+      .subscribe(data => {
+        this.news = data;
+        console.log('News data loaded', this.news);
+      });
+    }else{
+      console.log("Nema ida");
+    }
+  }
 
 }

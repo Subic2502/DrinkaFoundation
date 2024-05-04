2a0eeff81708dc526c00c580d32170ff777fc092
Subic2502 <uros.subic01@gmail.com>
2024-05-04 19:10:48 +0200

    conversion to base64 works

--- angular/src/app/news-writer/news-writer.component.ts
+++ angular/src/app/news-writer/news-writer.component.ts
@@ -9,7 +9,7 @@ interface FormData {
   Title: string;
   ShortText: string;
   LongText: string;
-  Photos: File[]
+  Photos: string[]
 }
 
 @Component({
@@ -26,7 +26,7 @@ export class NewsWriterComponent implements OnInit {
     Title: '',
     ShortText: '',
     LongText: '',
-    Photos: this.selectedFiles
+    Photos: []
   };
 
   constructor(private http: HttpClient) { }
@@ -46,20 +46,39 @@ export class NewsWriterComponent implements OnInit {
     this.selectedFiles = [];
   }
 
-  onSubmit() {
+  async onSubmit() {
+    //slike u base64
+    const base64Images: string[] = [];
+    console.log("Prvi log base64: " + base64Images)
+    for (const file of this.selectedFiles) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      await new Promise((resolve) => reader.onloadend = () => resolve(reader.result));
+      const base64String = reader.result?.toString()?.split(',')[1]; // Extract base64 data
+      console.log("Slika convertovana: "+base64String)
+      if (base64String) { // Check if base64 data is valid before pushing
+        base64Images.push(base64String);
+        console.log("Azuriran niz slika: "+base64Images)
+      }
+    }
+
+    // Update formData with base64 images
+    this.formData.Photos = base64Images;
+
+
     // Access form data in formData object
     console.log(this.formData);
 
-    this.http.post<boolean>('http://localhost:5024/api/News/add-news', this.formData)
-    .pipe(
-      catchError(error => {
-        console.error('Error', error);
-        return throwError(() => new Error('Error'));
-      })
-    )
-    .subscribe(Boolean => {
-      console.log('Added news to database');
-    });
+    // this.http.post<boolean>('http://localhost:5024/api/News/add-news', this.formData)
+    //   .pipe(
+    //     catchError(error => {
+    //       console.error('Error', error);
+    //       return throwError(() => new Error('Error'));
+    //     })
+    //   )
+    //   .subscribe(Boolean => {
+    //     console.log('Added news to database');
+    //   });
 
     // Optional: Clear form after submission (if desired)
     // this.formData = {

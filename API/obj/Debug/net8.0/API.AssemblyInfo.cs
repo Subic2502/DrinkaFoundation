a05834a69b94a575a278f0da491e24e392d6e027
Subic2502 <uros.subic01@gmail.com>
2024-05-08 13:08:11 +0200

    in progress

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6b76a24bfd252cc1367f9c423c736d3c170631b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19346af4889574a060902fbe4b3eb81b12c8a91d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
